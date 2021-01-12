fc702cef515165177240e803b29e43fe7ec25955
Breno Ribeiro do Val <brenor@finamax.com.br>
2021-01-12 11:59:31 -0300

    add restaurantService

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Restaurant } from './restaurant/restaurant.model';
+
+import { RestaurantsService } from './restaurants.service';
 
 @Component({
   selector: 'mt-restaurants',
@@ -7,28 +10,14 @@ import { Restaurant } from './restaurant/restaurant.model';
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurants: Restaurant[] = [
-    {
-      id: "bread-bakery",
-      name: "Bread & Bakery",
-      category: "Bakery",
-      deliveryEstimate: "25m",
-      rating: 4.9,
-      imagePath: "assets/img/restaurants/breadbakery.png",
-    },
-    {
-      id: "burger-house",
-      name: "Burger House",
-      category: "Hamburgers",
-      deliveryEstimate: "100m",
-      rating: 3.5,
-      imagePath: "assets/img/restaurants/burgerhouse.png",
-    }
-  ]
-  
-  constructor() { }
+  restaurants: Restaurant[];
+
+  constructor(private readonly restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
+    this.restaurantsService.restaurants().subscribe(restaurants => {
+      this.restaurants = restaurants;
+    });
   }
 
 }
