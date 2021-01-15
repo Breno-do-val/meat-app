535e35a2adac1fe93024ad9d3a7b37160b0ca8e3
Breno Ribeiro do Val <brenor@finamax.com.br>
2021-01-15 17:10:41 -0300

    (feat): add directive to hide component

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -10,14 +10,21 @@ import { RestaurantsService } from './restaurants.service';
 })
 export class RestaurantsComponent implements OnInit {
 
+  responseEmpty: boolean = true;
   restaurants: Restaurant[];
 
   constructor(private readonly restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
-    this.restaurantsService.restaurants().subscribe(restaurants => {
-      this.restaurants = restaurants;
-    });
+    this.restaurantsService.restaurants().subscribe(
+      restaurant => {
+        this.restaurants = restaurant;
+      },
+      err => {
+        console.log('Could not retrieve data from server');
+        this.responseEmpty = false;
+      }
+    );
   }
 
 }
